4204c10fadd21938e7d36b0fd2bf9d8f0640a99f
Guo Yixuan <culu.gyx@gmail.com>
2014-04-05 12:53:34 -0400

    Imported Upstream version 7.2.44+dfsg

--- clientgui/BOINCTaskBar.h
+++ clientgui/BOINCTaskBar.h
@@ -36,7 +36,7 @@ class CTaskbarEvent;
 
 class CTaskBarIcon : public wxTaskBarIconEx {
 public:
-    CTaskBarIcon(wxString title, wxIcon* icon, wxIcon* iconDisconnected, wxIcon* iconSnooze);
+    CTaskBarIcon(wxString title, wxIconBundle* icon, wxIconBundle* iconDisconnected, wxIconBundle* iconSnooze);
     ~CTaskBarIcon();
 
     void OnOpenWebsite(wxCommandEvent& event);
@@ -63,6 +63,8 @@ public:
 
     wxMenu *BuildContextMenu();
     void AdjustMenuItems(wxMenu* menu);
+
+    wxSize GetBestIconSize();
 
 #ifdef __WXMAC__
 private:
